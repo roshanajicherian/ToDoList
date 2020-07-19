5d9d6959f574a016a0b10f8b64414db033f982f4
roshanajicherian <roshanaji777@gmail.com>
2020-07-19 11:20:57 +0530

    Added basic html templates for basic functionallity testingf

--- ToDoList/base/views.py
+++ ToDoList/base/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+sampleToDos = [
+    {
+        'id': 1,
+        'content': 'Study DP',
+        'dateAdded': '24th August 2020',
+        'deadline': '30th August 2020'
+    },
+    {
+        'id': 2,
+        'content': 'Study Binary Seach',
+        'dateAdded': '15th August 2020',
+        'deadline': '23th August 2020'
+    }
+]
+
 
 def Home(request):
-    return render(request, 'base/home.html')
+    context = {
+        'toDos': sampleToDos,
+        'title': 'Home'
+    }
+    return render(request, 'base/home.html', context)
